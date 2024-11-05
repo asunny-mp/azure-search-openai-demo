3a0bbad644c401c942180f043c8626648168167e
Ajin Sunny <ajin.sunny@mindpetal.com>
2024-11-05 14:59:12 +0000

    (adjust) questions

--- locustfile.py
+++ locustfile.py
@@ -18,10 +18,9 @@ class ChatUser(HttpUser):
                     {
                         "content": random.choice(
                             [
-                                "What is included in my Northwind Health Plus plan that is not in standard?",
-                                "What does a Product Manager do?",
+                                "Where can I find my health insurance benefits?",
                                 "What happens in a performance review?",
-                                "Whats your whistleblower policy?",
+                                "What are the company holidays?",
                             ]
                         ),
                         "role": "user",
